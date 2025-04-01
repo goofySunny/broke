1f4c9f85b2d470bcecdccac946efb1b19366bc9e
Sunny <alinajafian580@Gmail.com>
2025-04-01 19:13:47 +0330

    App done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.awt.EventQueue;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        EventQueue.invokeLater(() -> {
+            new MainFrame();
+        });
     }
 }
