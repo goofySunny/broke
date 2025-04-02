96a2887c9015550b0f461541556719590de68122
Sunny <alinajafian580@Gmail.com>
2025-04-02 19:46:49 +0330

    Form panel update

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -4,6 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -22,6 +23,7 @@ public class FormPanel extends JPanel {
     JRadioButton male;
     JRadioButton female;
     ButtonGroup genderGroup;
+    JButton okButton;
 
     public FormPanel() {
         nameField = new JTextField();
@@ -32,24 +34,28 @@ public class FormPanel extends JPanel {
         employmentStatus.addItem("Unemployed");
 
         nationalNumber = new JTextField();
+        nationalNumber.setEnabled(false);
         localCitizen = new JCheckBox();
 
         // CheckBox enables or disables the nationalNumber textField
         localCitizen.addActionListener(e -> {
-            boolean checked = localCitizen.isEnabled();
+            boolean checked = localCitizen.isSelected();
             if (checked) {
-                nationalNumber.setEnabled(checked);
-                nationalNumber.setText("");
+                nationalNumber.setEnabled(true);
             } else {
                 nationalNumber.setEnabled(checked);
+                nationalNumber.setText("");
             }
         });
 
-        male = new JRadioButton();
-        female = new JRadioButton();
+        male = new JRadioButton("Male");
+        male.setSelected(true);
+        female = new JRadioButton("Female");
         genderGroup = new ButtonGroup();
         genderGroup.add(female);
         genderGroup.add(male);
+
+        okButton = new JButton("OK");
 
         setVisible(true);
         CompoundBorder border = new CompoundBorder(BorderFactory.createLineBorder(Color.BLACK), BorderFactory.createEmptyBorder(10,10,10,10));
@@ -65,7 +71,7 @@ public class FormPanel extends JPanel {
 
         gc.gridy = 0;
         gc.weightx = 1;
-        gc.weighty = 0.001;
+        gc.weighty = 0.01;
 
         // First row
         add(new JLabel("Name"), gc);
@@ -73,6 +79,55 @@ public class FormPanel extends JPanel {
         gc.gridx = 1;
         add(nameField, gc);
 
+
+        // Next row
+        gc.gridy++;
+        // Label
+        gc.gridx = 0;
+        add(new JLabel("Employment Status"), gc);
+        // Control
+        gc.gridx = 1;
+        add(employmentStatus, gc);
+
+        // Next row
+        gc.gridy++;
+        // Label
+        gc.gridx = 0;
+        add(new JLabel("Are you a local?"), gc);
+        // Control
+        gc.gridx = 1;
+        add(localCitizen, gc);
+
+        // Next row
+        gc.gridy++;
+        // Label
+        gc.gridx = 0;
+        add(new JLabel("National Number: "), gc);
+        // Control
+        gc.gridx = 1;
+        add(nationalNumber, gc);
+        
+        // Next row
+        gc.gridy++;
+        // Label
+        gc.gridx = 0;
+        add(new JLabel("Sex: "), gc);
+        // Control
+        gc.gridx = 1;
+        add(male, gc);
+
+        // Next row
+        gc.gridy++;
+        // Control
+        gc.gridx = 1;
+        add(female, gc);
+
+        // Next row
+        gc.gridy++;
+        // Ok Button
+        gc.weighty = 1;
+        gc.gridx = 1;
+        add(okButton, gc);
     }
 
 }
