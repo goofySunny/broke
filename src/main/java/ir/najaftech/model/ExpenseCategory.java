6e3220045b9fc0c9b539777fdcf0e861a0b6bc7b
GoofySunny <Alinajafian580@gmail.com>
2025-05-17 11:47:55 +0330

    Overrided the toString method

--- src/main/java/ir/najaftech/model/ExpenseCategory.java
+++ src/main/java/ir/najaftech/model/ExpenseCategory.java
@@ -5,5 +5,12 @@ public enum ExpenseCategory {
     RELATIONSHIP,
     FOOD,
     DEBT,
-    OTHER
+    OTHER;
+
+    @Override
+    public String toString() {
+        String sb = name().charAt(0) +
+                name().substring(1).toLowerCase();
+        return sb;
+    }
 }
