1d504e3735b57bc8e020e3a93448be9df58df031
GoofySunny <Alinajafian580@gmail.com>
2025-05-18 17:13:37 +0330

    Minor change

--- src/main/java/ir/najaftech/gui/GraphPanel.java
+++ src/main/java/ir/najaftech/gui/GraphPanel.java
@@ -13,6 +13,7 @@ public class GraphPanel extends JPanel {
     ArrayList<Expense> expenses;
     JList<Expense> expenseJList;
     DefaultListModel<Expense> listModel;
+    JLabel expenseCounter;
 //    Members
 
     public GraphPanel(ArrayList<Expense> expenses) {
@@ -27,15 +28,31 @@ public class GraphPanel extends JPanel {
         expenseJList = new JList<>();
         expenseJList.setModel(listModel);
 
+        expenseCounter = new JLabel("Current expense count: " + expenses.size());
 
-        add(expenseJList);
+//        add(expenseJList);
 //        Set up self
         setLayout(new GridBagLayout());
         setMinimumSize(new Dimension(350, 500));
+
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.weightx = 0.3;
+        gc.weighty = 0.3;
+
+//        First Row
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(expenseCounter, gc);
+
+
         setVisible(true);
     }
 
     void addExpense(Expense e) {
+        expenses.add(e);
         listModel.addElement(e);
+        expenseCounter.setText("Current expense count: " + expenses.size());
     }
 }
