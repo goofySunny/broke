5d035ea34002ea452e600e4543cbf803b0eb4f53
bigali <Alinajafian580@gmail.com>
2025-08-21 16:52:54 +0330

    Only allow selection of one row at a time

--- src/main/java/ir/najaftech/util/CustomJTable.java
+++ src/main/java/ir/najaftech/util/CustomJTable.java
@@ -6,6 +6,7 @@ public class CustomJTable extends JTable {
 
     public CustomJTable(Object[][] data, Object[] columns) {
         super(data, columns);
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     }
 
     @Override
